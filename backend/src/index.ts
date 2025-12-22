6d26215cf46920ced180447c5a30301453e273b8
Igor <igor.gds.00@gmail.com>
2025-12-22 18:06:52 -0300

    Adiciona middleware de autenticação

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import express from "express";
 import { buildSchema } from "type-graphql";
 import { AuthResolver } from "./resolvers/auth.resolver";
 import { UserResolver } from "./resolvers/user.resolver";
+import { buildContext } from "./graphql/context";
 
 async function bootstrap() {
   const app = express()
@@ -22,7 +23,7 @@ async function bootstrap() {
 
   await server.start()
 
-  app.use("/graphql", express.json(), expressMiddleware(server))
+  app.use("/graphql", express.json(), expressMiddleware(server, { context: buildContext }))
 
   app.listen(
     { port: 4000 },
--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -18,3 +18,8 @@ export const signJwt = (payload: JwtPayload, expiresIn?: string): string => {
 
   return jwt.sign(payload, secret, options)
 }
+
+export const verifyJwt = (token: string): JwtPayload => {
+  const secret: Secret = process.env.JWT_SECRET as unknown as Secret
+  return jwt.verify(token, secret) as JwtPayload
+}
