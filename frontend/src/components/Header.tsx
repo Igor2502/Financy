8be661d259d38cb581f3061ba627fbdb3f401b93
Igor <igor.gds.00@gmail.com>
2026-01-22 18:23:09 -0300

    Atualiza o estilo do header conforme pagina acessada

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,10 +1,15 @@
 import { useAuthStore } from "@/stores/auth"
 
 import logoIcon from "@/assets/logo.svg"
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 
 export function Header() {
   const { user, isAuthenticated } = useAuthStore()
+
+  const location = useLocation()
+  const isDashboardPage = location.pathname === "/"
+  const isTransactionPage = location.pathname === "/transactions"
+  const isCategoryPage = location.pathname === "/categories"
 
   return (
     <>
@@ -15,16 +20,22 @@ export function Header() {
               <img src={logoIcon} alt="Logo" className="h-6 w-[100px]" />
             </div>
             <div className="flex items-center gap-4">
-              <Link to="/" className="text-brand-base font-medium hover:text-gray-700 hover:font-medium">
-                Dashboard
-              </Link>
-              <Link to="/transactions" className="text-gray-600 hover:text-gray-700 hover:font-medium">
-                Transações
-              </Link>
-              <Link to="/categories" className="text-gray-600 hover:text-gray-700 hover:font-medium">
-                Categorias
-              </Link>
+              <Link to="/" className={`${isDashboardPage
+                ? "text-brand-base font-medium hover:text-gray-700 hover:font-medium"
+                : "text-gray-600 hover:text-gray-700 hover:font-medium"}`}
+              >Dashboard</Link>
+
+              <Link to="/transactions" className={`${isTransactionPage
+                ? "text-brand-base font-medium hover:text-gray-700 hover:font-medium"
+                : "text-gray-600 hover:text-gray-700 hover:font-medium"}`}
+              >Transações</Link>
+
+              <Link to="/categories" className={`${isCategoryPage
+                ? "text-brand-base font-medium hover:text-gray-700 hover:font-medium"
+                : "text-gray-600 hover:text-gray-700 hover:font-medium"}`}
+              >Categorias</Link>
             </div>
+
             <div className="bg-gray-200 p-2 rounded-3xl w-9 h-9 flex items-center justify-center">
               <span className="font-medium text-gray-800h">{user?.name?.slice(0, 2)?.toUpperCase()}</span>
             </div>
